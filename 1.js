452b96a13e0708299a5138b57f85ee2b3f13a81b
Ngo Quang Du <hondufun@gmail.com>
2020-09-19 13:52:38 +0700

    gui lai slide da sua

--- 1.js
+++ 1.js
@@ -2,9 +2,13 @@ document.addEventListener("DOMContentLoaded",function(){
 	var menu = document.querySelectorAll('.khoi1-menu'), // đặt tên khối slide cần chuyển đúng class này
 		nutDuoi = document.querySelectorAll('.khoi1-nut ul li'), // đặt tên các nút nhỏ số lượng tương ứng với số slide
 		nutTrai = document.querySelector('.khoi1-trai'), // tên cho nút bên trái
-		nutPhai = document.querySelector('.khoi1-phai'); // tên cho nút bên phải
+		nutPhai = document.querySelector('.khoi1-phai'), // tên cho nút bên phải
+		trangThai = "tinh",
+		giaTri = 0;
 	for(i = 0;i < nutDuoi.length; i++){
 		nutDuoi[i].onclick = function(){
+			var nutMenu = document.querySelector('.hienMenu');
+			clearInterval(auto);
 			for (var j = 0; j < nutDuoi.length; j++) {
 				nutDuoi[j].classList.remove('mauNut')
 			}
@@ -13,79 +17,140 @@ document.addEventListener("DOMContentLoaded",function(){
 			var viTriClick = this,
 				csht = 0;
 			for(csht = 0;viTriClick  = viTriClick.previousElementSibling;csht++){};
-			for (var i = 0; i < menu.length; i++) {
-				menu[i].classList.remove('hienMenu')
+			for (var l = 0; l < menu.length; l++) {
+				menu[l].classList.remove('hienMenu')
 			}
-			menu[csht].classList.add('hienMenu')
-			clearInterval(auto)
+			nutMenu.classList.add("bienMat")
+			menu[csht].classList.add('hienRa')
+			nutMenu.addEventListener('webkitAnimationEnd', function(){
+				this.classList.remove('bienMat')
+				this.classList.remove('hienMenu')
+			})
+			menu[csht].addEventListener('webkitAnimationEnd', function(){
+				this.classList.remove('hienRa')
+				this.classList.add('hienMenu')
+			})
+			console.log(nutMenu)
 		}// xong nut nut
 	}	// toi nut 2 ben
 	nutTrai.onclick = function(){
-		var viTriGoc = document.querySelector('.hienMenu');
-			vtTruoc = viTriGoc.previousElementSibling;
-		for(chiSoTruoc = 0; viTriGoc = viTriGoc.previousElementSibling; chiSoTruoc++){}
-			chiSoTruoc = chiSoTruoc-1; //lay chi so cho may cai nut duoi
-		if(chiSoTruoc < 0){
-			for (var i = 0; i < menu.length; i++) {
-				menu[i].classList.remove('hienMenu')
+		clearInterval(auto);
+		if(trangThai == "dong"){return false}
+		else{
+			trangThai == "dong";
+			var vtGoc = document.querySelector('.hienMenu'),
+				vtHienTai = document.querySelector('.hienMenu'),
+				vtTruoc = vtGoc.previousElementSibling,
+				chiSo = 0;
+		// xu ly kep cho nut duoi
+			for(chiSo = 0; vtGoc = vtGoc.previousElementSibling; chiSo++){}
+			chiSo = chiSo-1;
+			for (var i = 0; i < nutDuoi.length; i++) {
 				nutDuoi[i].classList.remove('mauNut')
 			}
-			menu[(menu.length-1)].classList.add('hienMenu')
-			nutDuoi[(menu.length-1)].classList.add('mauNut')
-		}else{
-			for (var i = 0; i < menu.length; i++) {
-				menu[i].classList.remove('hienMenu')
-				nutDuoi[i].classList.remove('mauNut')
+			if(chiSo < 0){
+				nutDuoi[(menu.length-1)].classList.add('mauNut')
+				menu[(menu.length-1)].classList.add('hienRa')
+				menu[(menu.length-1)].addEventListener('webkitAnimationEnd', function(){
+					this.classList.add('hienMenu')
+					this.classList.remove('hienRa')
+				})
+				giaTri++
+			}else{
+		// xu ly bien mat
+				nutDuoi[chiSo].classList.add('mauNut')
+		// xu ly hien ra
+				vtTruoc.classList.add('hienRa')
+				vtTruoc.addEventListener('webkitAnimationEnd', function(){
+					this.classList.add('hienMenu')
+					this.classList.remove('hienRa')
+				})
+				giaTri++;
 			}
-			vtTruoc.classList.add('hienMenu')
-			nutDuoi[chiSoTruoc].classList.add('mauNut')
-			clearInterval(auto)
-		}	
-	}// toi nut phai
+			vtHienTai.classList.add('bienMat')
+			var xlHienTai = function(){
+					this.classList.remove('hienMenu');
+					this.classList.remove('bienMat');
+					console.log("msg")
+					giaTri++;
+					if(giaTri == 2){
+						trangThai = "tinh";
+						giaTri = 0;
+				}
+			}
+			vtHienTai.addEventListener('webkitAnimationEnd', xlHienTai)
+			console.log("xam")
+		}
+	}
+	// toi nut phai
 	nutPhai.onclick = function(){
-		var viTriGoc = document.querySelector('.hienMenu');
-			vtSau = viTriGoc.nextElementSibling;
-			chiSoSau = 0;
-		for(chiSoSau = 0; viTriGoc = viTriGoc.previousElementSibling; chiSoSau++){}
-			chiSoSau = chiSoSau+1; //lay chi so cho may cai nut duoi
-		if(chiSoSau == menu.length){
-			for (var i = 0; i < menu.length; i++) {
-				menu[i].classList.remove('hienMenu')
-				nutDuoi[i].classList.remove('mauNut')
-			}
-			menu[0].classList.add('hienMenu')
+		clearInterval(auto);
+		var vtGoc = document.querySelector('.hienMenu'),
+		vtSau = vtGoc.nextElementSibling,
+			vtHienTai = vtGoc,
+			chiSo = 0;
+	// xu ly kep cho nut duoi
+		for(chiSo = 0; vtGoc = vtGoc.previousElementSibling; chiSo++){}
+		chiSo = chiSo+1;
+		for (var i = 0; i < nutDuoi.length; i++) {
+			nutDuoi[i].classList.remove('mauNut')
+		}
+		if(chiSo > (menu.length-1)){
 			nutDuoi[0].classList.add('mauNut')
+			menu[0].classList.add('hienRa')
+			menu[0].addEventListener('webkitAnimationEnd', function(){
+				this.classList.add('hienMenu')
+				this.classList.remove('hienRa')
+			})
 		}else{
-			for (var i = 0; i < menu.length; i++) {
-				menu[i].classList.remove('hienMenu')
-				nutDuoi[i].classList.remove('mauNut')
-			}
-			vtSau.classList.add('hienMenu')
-			nutDuoi[chiSoSau].classList.add('mauNut')
-			clearInterval(auto)
-		}	
-	}	// toi auto slide
+			nutDuoi[chiSo].classList.add('mauNut')
+			// xu ly sau
+			vtSau.classList.add('hienRa')
+			vtSau.addEventListener('webkitAnimationEnd', function(){
+				this.classList.add('hienMenu')
+				this.classList.remove('hienRa')
+			})
+		}
+		vtHienTai.classList.add('bienMat')
+		vtHienTai.addEventListener('webkitAnimationEnd', function(){
+			this.classList.remove('hienMenu')
+			this.classList.remove('bienMat')
+		})
+	}
+
+
 	var auto = setInterval(function(){
-		var viTriGoc = document.querySelector('.hienMenu');
-			vtSau = viTriGoc.nextElementSibling;
-			chiSoSau = 0;
-		for(chiSoSau = 0; viTriGoc = viTriGoc.previousElementSibling; chiSoSau++){}
-			chiSoSau = chiSoSau+1; //lay chi so cho may cai nut duoi
-		if(chiSoSau == menu.length){
-			for (var i = 0; i < menu.length; i++) {
-				menu[i].classList.remove('hienMenu')
-				nutDuoi[i].classList.remove('mauNut')
-			}
-			menu[0].classList.add('hienMenu')
+		var vtGoc = document.querySelector('.hienMenu'),
+		vtSau = vtGoc.nextElementSibling,
+			vtHienTai = vtGoc,
+			chiSo = 0;
+	// xu ly kep cho nut duoi
+		for(chiSo = 0; vtGoc = vtGoc.previousElementSibling; chiSo++){}
+		chiSo = chiSo+1;
+		for (var i = 0; i < nutDuoi.length; i++) {
+			nutDuoi[i].classList.remove('mauNut')
+		}
+		if(chiSo > (menu.length-1)){
 			nutDuoi[0].classList.add('mauNut')
+			menu[0].classList.add('hienRa')
+			menu[0].addEventListener('webkitAnimationEnd', function(){
+				this.classList.add('hienMenu')
+				this.classList.remove('hienRa')
+			})
 		}else{
-			for (var i = 0; i < menu.length; i++) {
-				menu[i].classList.remove('hienMenu')
-				nutDuoi[i].classList.remove('mauNut')
-			}
-			vtSau.classList.add('hienMenu')
-			nutDuoi[chiSoSau].classList.add('mauNut')
-		}	
+			nutDuoi[chiSo].classList.add('mauNut')
+			// xu ly sau
+			vtSau.classList.add('hienRa')
+			vtSau.addEventListener('webkitAnimationEnd', function(){
+				this.classList.add('hienMenu')
+				this.classList.remove('hienRa')
+			})
+		}
+		vtHienTai.classList.add('bienMat')
+		vtHienTai.addEventListener('webkitAnimationEnd', function(){
+			this.classList.remove('hienMenu')
+			this.classList.remove('bienMat')
+		})
 	},2000)
 },false)
 
